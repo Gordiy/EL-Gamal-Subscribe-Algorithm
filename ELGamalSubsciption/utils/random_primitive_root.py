30413990764b4dc9a2c950b8be78d259b7a63d8d
hordii.rushynets <hordii.rushynets@globallogic.com>
2022-01-31 06:32:27 +0200

    Add: function that obtain random primitive root for modulo number

---
+++ ELGamalSubsciption/utils/random_primitive_root.py
@@ -0,0 +1,24 @@
+from random import randint
+
+
+def _gcd(a: int,b: int) -> int:
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def random_primitive_root(modulo: int) -> int:
+    """
+    Get random primitive root.
+
+    :param modulo:
+    :return:
+    """
+    p_root = []
+    required_set = set(num for num in range (1, modulo) if _gcd(num, modulo) == 1)
+    for g in range(1, modulo):
+        actual_set = set(pow(g, powers) % modulo for powers in range (1, modulo))
+        if required_set == actual_set:
+            p_root.append(g)
+
+    return p_root[randint(0, len(p_root)-1)]
