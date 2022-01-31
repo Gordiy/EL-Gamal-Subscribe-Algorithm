fa052e68a3e51b715a2979b080c6fe411e42c0cc
hordii.rushynets <hordii.rushynets@globallogic.com>
2022-01-31 05:21:19 +0200

    Initialize El Gamal Subscription Algorithm

---
+++ TextFileIO/constants.py
@@ -0,0 +1,9 @@
+"""Constans needed for package that write and read data to/from file."""
+
+import os
+
+filename_read = 'readfile.txt'
+filename_write = 'writefile.txt'
+
+path_to_file_read = f"{os.getcwd()}/temp/{filename_read}"
+path_to_file_write = f"{os.getcwd()}/temp/{filename_write}"
