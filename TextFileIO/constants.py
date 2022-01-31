c80b8d25ab71dacd10ee6556abd7407f814e12d5
hordii.rushynets <hordii.rushynets@globallogic.com>
2022-01-31 05:41:04 +0200

    Implement method that read and write data from/to file

--- TextFileIO/constants.py
+++ TextFileIO/constants.py
@@ -1,9 +1,9 @@
 """Constans needed for package that write and read data to/from file."""
-
 import os
+
 
 filename_read = 'readfile.txt'
 filename_write = 'writefile.txt'
 
-path_to_file_read = f"{os.getcwd()}/temp/{filename_read}"
-path_to_file_write = f"{os.getcwd()}/temp/{filename_write}"
+path_to_file_read = f"{os.getcwd()}/temp/read/{filename_read}"
+path_to_file_write = f"{os.getcwd()}/temp/write/{filename_write}"
--- TextFileIO/text_file_io.py
+++ TextFileIO/text_file_io.py
@@ -1,2 +1,17 @@
+"""Working with file."""
+from .constants import path_to_file_read, path_to_file_write
+
+
 class TextFileIO:
-    pass
+    """Definition methods that read and write data from/to file."""
+    @staticmethod
+    def file_read():
+        file = open(f"{path_to_file_read}.txt", 'r')
+        return file.read()
+
+    @staticmethod
+    def file_write(data, data_and_time):
+        """Write data to file or append data to exist file."""
+        file = open(f"{path_to_file_write}{data_and_time}.txt", 'a')
+        file.write(f"{data}\n")
+        file.close()
